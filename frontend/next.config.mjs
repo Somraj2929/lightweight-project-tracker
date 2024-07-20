0177dd009cad0f35d99062b50e41cb603dda91b2
Somraj <somrajbishnoirock29@gmail.com>
2024-07-20 05:51:50 +0530

    chore: Update .gitignore to include frontend and backend .env files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["d2u8k2ocievbld.cloudfront.net"],
+    domains: [
+      "d2u8k2ocievbld.cloudfront.net",
+      "somraj-project-tracker.s3.amazonaws.com",
+    ],
   },
 };
 
