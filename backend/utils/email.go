dee38d5039533ecb8fc8fb2b00849be649846e68
Somraj <somrajbishnoirock29@gmail.com>
2024-08-09 03:13:34 +0530

    chore: Update email template for password reset

--- backend/utils/email.go
+++ backend/utils/email.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/smtp"
 	"os"
+	"fmt"
 )
 
 func SendPasswordResetEmail(email, token string) {
@@ -11,12 +12,34 @@ func SendPasswordResetEmail(email, token string) {
 	password := os.Getenv("RESET_EMAIL_PASSWORD")
 	to := email
 	subject := "Password Reset Request"
-	body := "Click the link to reset your password: http://localhost:3000/users/reset-password?token=" + token
+	body := fmt.Sprintf(`
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Password Reset Request</title>
+	</head>
+	<body>
+		<p>Dear User,</p>
+		<p>You requested to reset your password. Please click the link below to reset your password:</p>
+		<p>
+			<a href="https://lightweight-project-tracker.vercel.app/users/reset-password?token=%s" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; border-radius: 5px;">
+				Reset Password
+			</a>
+		</p>
+		<p>If you did not request this change, please ignore this email.</p>
+		<p>Thank you,<br>Your Company Name</p>
+	</body>
+	</html>
+	`, token)
 
 	message := []byte("From: " + from + "\r\n" +
 		"To: " + to + "\r\n" +
 		"Subject: " + subject + "\r\n" +
+		"MIME-version: 1.0;\r\n" +
+		"Content-Type: text/html; charset=\"UTF-8\";\r\n" +
 		"\r\n" + body + "\r\n")
+
 	auth := smtp.PlainAuth("", from, password, "us2.smtp.mailhostbox.com")
 
 	err := smtp.SendMail("us2.smtp.mailhostbox.com:587", auth, from, []string{to}, message)
