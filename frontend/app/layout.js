2fb25bf1238f55c67e96b735d3019033f7119eff
Somraj <somrajbishnoirock29@gmail.com>
2024-09-07 18:48:17 +0530

    Add Simple Analytics tracking script to project layout

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         {children}
         <SpeedInsights />
         <Analytics />
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
       </body>
     </html>
   );
